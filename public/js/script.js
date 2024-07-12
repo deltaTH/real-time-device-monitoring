e941559edca59c8fd15b77589c71ba0219d6346f
Abhay Singh <as933155@gmail.com>
2024-07-12 13:11:59 +0530

    love you cutie ji

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+const socket = io();
+
+console.log("heyyy");
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
+    }, (error) => {
+        console.error(error);
+    },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+
+}
+
+L.map("map").setView([0,0], 10);
